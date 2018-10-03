004f4144405280b10e5344b85c6cb4650f6f536c
Andrew Bernier <a6bernier@gmail.com>
2018-10-03 21:37:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ class TicTacToe
       index.between?(0,8) && !position_taken?(index)
     end
 
-  def turn
+  def turn(input, index, current_player)
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
