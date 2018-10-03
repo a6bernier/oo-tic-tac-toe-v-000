3d77d893720acf88f2cc1d13ebcb5a7a60d07be1
Andrew Bernier <a6bernier@gmail.com>
2018-10-03 21:51:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,7 +102,7 @@ class TicTacToe
       return true
     elsif full? == true && won? != false
       return true
-    elsif full? == false && won?(@board) != false
+    elsif full? == false && won? != false
       return true
     else
       false
