dbeaf81a0776c8e71b7c10c496700007c2fb7946
Andrew Bernier <a6bernier@gmail.com>
2018-10-03 21:03:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,10 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    if index.between?(0,8) && !position_taken?(@board, index)
+      true
+    else
+      false
   end
 
   def turn
