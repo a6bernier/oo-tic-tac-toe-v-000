257a03107f5fb27e393def8cffce3316b64fda03
Andrew Bernier <a6bernier@gmail.com>
2018-10-03 20:25:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,10 @@
 class TicTacToe
 
+  def initialize(board)
+    @board = [ " ", " ",  " ",  " ",  " ",  " ",  " ",  " ", " " ]
+  end
+
+
   WIN_COMBINATIONS = [
     [0,1,2], #top
     [3,4,5], #Middle
@@ -10,6 +15,8 @@ class TicTacToe
     [2,4,6], #diagonal
     [0,4,8]  #diagonal
   ]
+
+
 
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
